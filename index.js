0a5b1e345ea496edfeaec526c52bcfa3db1f80a6
younes <youn.henni@gmail.com>
2022-12-17 15:45:08 +0100

    Adds EJS

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.set('views', 'public');
+
+app.get('/:name/:email', (req, res) => {
+  res.render('index', req.params) 
+})
+app.get('/:name', (req, res) => {
+  res.render('index', {name: req.params.name, email: "unspecified"})
+})
 
 app.listen(3000);
